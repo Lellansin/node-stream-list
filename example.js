60deefc68e931a5d9fe8b27e218b1f66fa307c4c
Lellansin <lellansin@gmail.com>
2014-09-30 06:32:04 +0800

    modified example

--- example.js
+++ example.js
@@ -1,15 +1,14 @@
-#!/usr/bin/env node
-
+var underscore = require('underscore');
 var ForkStream = require("./");
 
 var fork = new ForkStream({
   classifier: function classify(e, done) {
-    return done(null, e >= 5);
+    return done(null, underscore.random(0,1));
   },
 });
 
-fork.a.on("data", console.log.bind(null, "a"));
-fork.b.on("data", console.log.bind(null, "b"));
+fork.list[0].on("data", console.log.bind(null, "a"));
+fork.list[1].on("data", console.log.bind(null, "b"));
 
 for (var i=0;i<20;++i) {
   fork.write(Math.round(Math.random() * 10));
